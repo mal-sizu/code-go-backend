1fc158ffcd6c4271de5c5b91a7efda2f5a8dbbe5
prageeth <prageethravindra1432@gmail.com>
2025-05-16 18:01:33 +0530

    fix comments issues

--- src/main/java/com/dynac/plantation/model/Comment.java
+++ src/main/java/com/dynac/plantation/model/Comment.java
@@ -1,10 +1,15 @@
 package com.dynac.plantation.model;
 
 import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import java.time.LocalDateTime;
 
 @Data
+@Document(collection = "comments")
 public class Comment {
+    @Id
     private String id;
     private String postId;
     private String userId;
@@ -12,4 +17,5 @@ public class Comment {
     private String userProfileImage;
     private String content;
     private LocalDateTime createdAt = LocalDateTime.now();
+    private LocalDateTime updatedAt = LocalDateTime.now();
 }
--- src/main/java/com/dynac/plantation/repository/CommentRepository.java
+++ src/main/java/com/dynac/plantation/repository/CommentRepository.java
@@ -3,6 +3,7 @@ package com.dynac.plantation.repository;
 import com.dynac.plantation.model.Comment;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
 @Repository
